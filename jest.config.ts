e6553447bbf3c768f4e963ce4c9a53f5dafde464
Siddharth Nair <nairsid2@gmail.com>
2023-08-06 09:59:25 -0400

    Add jest, rtl, jsdom setup and config to project

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+import nextJest from "next/jest";
+import { type } from "os";
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const customJestConfig = {
+  moduleDirectories: ["node_modules", "<rootDir>/"],
+  testEnvironment: "jest-environment-jsdom",
+};
+module.exports = createJestConfig(customJestConfig);
